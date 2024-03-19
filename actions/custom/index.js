272ab4f6fb421ca9cea2b7596bcdde8e8153a8d8
Mohitdj794 <mohit.krishnanraju@ziffity.com>
2024-03-19 11:32:39 +0530

    initial commit

---
+++ actions/custom/index.js
@@ -0,0 +1,46 @@
+/*
+* <license header>
+*/
+
+/**
+ * This is a sample action showcasing how to access an external API
+ *
+ * Note:
+ * You might want to disable authentication and authorization checks against Adobe Identity Management System for a generic action. In that case:
+ *   - Remove the require-adobe-auth annotation for this action in the manifest.yml of your application
+ *   - Remove the Authorization header from the array passed in checkMissingRequestInputs
+ *   - The two steps above imply that every client knowing the URL to this deployed action will be able to invoke it without any authentication and authorization checks against Adobe Identity Management System
+ *   - Make sure to validate these changes against your security requirements before deploying the action
+ */
+
+
+const fetch = require('node-fetch')
+const { Core } = require('@adobe/aio-sdk')
+const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+
+// main function that will be executed by Adobe I/O Runtime
+async function main (params) {
+  const logger = Core.logger('main',{level:params.LOG_LEVEL ||'info'})
+  try{
+
+    logger.info('calling the hello action ');
+    let message = "Hellow world"
+    logger.info(`the name parameter is: ${params.name}`);
+
+    if (!params.name){
+      message = `${message},my name is ${params.name}`
+    }
+
+    const body = {
+      message
+    }
+  }
+  catch (error) {
+    // log any server errors
+    logger.error(error)
+    // return with 500
+    return errorResponse(500, 'server error', logger)
+  }
+}
+
+exports.main = main
